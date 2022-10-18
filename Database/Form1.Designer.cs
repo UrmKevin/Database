147a38afae6d3fbd96c55c4370ed075c096644de
opilane <opilane@HP-CZC5261P8F>
2022-10-18 14:12:57 +0300

    Add project files.

---
+++ Database/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Toode_txt = new System.Windows.Forms.TextBox();
+            this.Hind_txt = new System.Windows.Forms.TextBox();
+            this.Kogus_txt = new System.Windows.Forms.TextBox();
+            this.Toode_lbl = new System.Windows.Forms.Label();
+            this.Hind_lbl = new System.Windows.Forms.Label();
+            this.Kogus_lbl = new System.Windows.Forms.Label();
+            this.Kategooria_lbl = new System.Windows.Forms.Label();
+            this.Kategooria_cbx = new System.Windows.Forms.ComboBox();
+            this.Toode_pbx = new System.Windows.Forms.PictureBox();
+            this.KogusPic_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Toode_pbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Toode_txt
+            // 
+            this.Toode_txt.Location = new System.Drawing.Point(127, 37);
+            this.Toode_txt.Name = "Toode_txt";
+            this.Toode_txt.Size = new System.Drawing.Size(152, 20);
+            this.Toode_txt.TabIndex = 0;
+            // 
+            // Hind_txt
+            // 
+            this.Hind_txt.Location = new System.Drawing.Point(127, 108);
+            this.Hind_txt.Name = "Hind_txt";
+            this.Hind_txt.Size = new System.Drawing.Size(152, 20);
+            this.Hind_txt.TabIndex = 1;
+            // 
+            // Kogus_txt
+            // 
+            this.Kogus_txt.Location = new System.Drawing.Point(127, 73);
+            this.Kogus_txt.Name = "Kogus_txt";
+            this.Kogus_txt.Size = new System.Drawing.Size(152, 20);
+            this.Kogus_txt.TabIndex = 2;
+            // 
+            // Toode_lbl
+            // 
+            this.Toode_lbl.AutoSize = true;
+            this.Toode_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Toode_lbl.Location = new System.Drawing.Point(14, 36);
+            this.Toode_lbl.Name = "Toode_lbl";
+            this.Toode_lbl.Size = new System.Drawing.Size(107, 18);
+            this.Toode_lbl.TabIndex = 3;
+            this.Toode_lbl.Text = "Toode nimetus";
+            // 
+            // Hind_lbl
+            // 
+            this.Hind_lbl.AutoSize = true;
+            this.Hind_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Hind_lbl.Location = new System.Drawing.Point(83, 108);
+            this.Hind_lbl.Name = "Hind_lbl";
+            this.Hind_lbl.Size = new System.Drawing.Size(38, 18);
+            this.Hind_lbl.TabIndex = 4;
+            this.Hind_lbl.Text = "Hind";
+            // 
+            // Kogus_lbl
+            // 
+            this.Kogus_lbl.AutoSize = true;
+            this.Kogus_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Kogus_lbl.Location = new System.Drawing.Point(70, 73);
+            this.Kogus_lbl.Name = "Kogus_lbl";
+            this.Kogus_lbl.Size = new System.Drawing.Size(51, 18);
+            this.Kogus_lbl.TabIndex = 5;
+            this.Kogus_lbl.Text = "Kogus";
+            // 
+            // Kategooria_lbl
+            // 
+            this.Kategooria_lbl.AutoSize = true;
+            this.Kategooria_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Kategooria_lbl.Location = new System.Drawing.Point(41, 144);
+            this.Kategooria_lbl.Name = "Kategooria_lbl";
+            this.Kategooria_lbl.Size = new System.Drawing.Size(80, 18);
+            this.Kategooria_lbl.TabIndex = 6;
+            this.Kategooria_lbl.Text = "Kategooria";
+            // 
+            // Kategooria_cbx
+            // 
+            this.Kategooria_cbx.FormattingEnabled = true;
+            this.Kategooria_cbx.Location = new System.Drawing.Point(127, 141);
+            this.Kategooria_cbx.Name = "Kategooria_cbx";
+            this.Kategooria_cbx.Size = new System.Drawing.Size(152, 21);
+            this.Kategooria_cbx.TabIndex = 7;
+            // 
+            // Toode_pbx
+            // 
+            this.Toode_pbx.Location = new System.Drawing.Point(306, 36);
+            this.Toode_pbx.Name = "Toode_pbx";
+            this.Toode_pbx.Size = new System.Drawing.Size(186, 148);
+            this.Toode_pbx.TabIndex = 8;
+            this.Toode_pbx.TabStop = false;
+            // 
+            // KogusPic_btn
+            // 
+            this.KogusPic_btn.Location = new System.Drawing.Point(498, 154);
+            this.KogusPic_btn.Name = "KogusPic_btn";
+            this.KogusPic_btn.Size = new System.Drawing.Size(90, 30);
+            this.KogusPic_btn.TabIndex = 9;
+            this.KogusPic_btn.Text = "Otsi fail";
+            this.KogusPic_btn.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Kustuta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Uuenda";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 30);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Lisa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 254);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(620, 251);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 517);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.KogusPic_btn);
+            this.Controls.Add(this.Toode_pbx);
+            this.Controls.Add(this.Kategooria_cbx);
+            this.Controls.Add(this.Kategooria_lbl);
+            this.Controls.Add(this.Kogus_lbl);
+            this.Controls.Add(this.Hind_lbl);
+            this.Controls.Add(this.Toode_lbl);
+            this.Controls.Add(this.Kogus_txt);
+            this.Controls.Add(this.Hind_txt);
+            this.Controls.Add(this.Toode_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Toode_pbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Toode_txt;
+        private System.Windows.Forms.TextBox Hind_txt;
+        private System.Windows.Forms.TextBox Kogus_txt;
+        private System.Windows.Forms.Label Toode_lbl;
+        private System.Windows.Forms.Label Hind_lbl;
+        private System.Windows.Forms.Label Kogus_lbl;
+        private System.Windows.Forms.Label Kategooria_lbl;
+        private System.Windows.Forms.ComboBox Kategooria_cbx;
+        private System.Windows.Forms.PictureBox Toode_pbx;
+        private System.Windows.Forms.Button KogusPic_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Database/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\Tooded_DB.mdf;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        public Form1()
+        {
+            InitializeComponent();
+            Naita_Andmed();
+        }
+        public void Naita_Andmed()
+        {
+            connect.Open();
+            DataTable dt = new DataTable();
+            //cmd = new SqlCommand("SELECT * FROM Toodetable");
+            adapter = new SqlDataAdapter("SELECT * FROM Toodetable", connect);
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connect.Close();
+        }
+    }
+}
